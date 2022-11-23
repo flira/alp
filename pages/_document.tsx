fa71cddbe81d9867b00e3b1d0b188f734f844e88
Fernando Lira <fernando@lira.etc.br>
2022-11-23 20:54:55 -0300

    Accessibility optimization

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,11 +2,14 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class CustomDocument extends Document {
+
   render() {
     return (
-      <Html>
+      <Html lang='pt-br'>
         <Head>
-          <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,400;0,700;0,800;1,400;1,700&display=optional" rel="stylesheet" />
+          <style>
+            @import url('https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,400;0,700;0,800;1,400&display=swap');
+          </style>
         </Head>
         <body>
           <Main />
